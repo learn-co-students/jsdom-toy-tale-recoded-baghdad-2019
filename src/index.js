7d54522087699238fb00925feef58507df143f71
K3BE <ahmed.13.alkaaby@gmail.com>
2019-10-22 13:40:29 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -16,3 +16,31 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+let url="http://localhost:3000/toys";
+fetch(url,
+{ headers: {
+  'Content-Type':'application/json',
+  'Accept': 'application/json'
+},
+method: "GET"}
+).then(resp => resp.json())
+.then(function(toys){
+  let toysDiv=document.querySelector("#toy-collection");
+  for (let toy of toys){
+    function f(id,name,image,likes){
+      this.id=id;
+      this.name=name;
+      this.image=image;
+      this.likes=likes;
+    }
+    let newDiv=document.createElement('div');
+    let elemObj=new f(toy.id,toy.name,toy.image,toy.likes);
+    newDiv.class="card";
+    console.log(elemObj);
+    newDiv.innerHTML=JSON.stringify(elemObj);
+    JSON.stringify(newDiv);
+
+    console.log(newDiv);
+    toysDiv.appendChild(newDiv);
+  }
+})
