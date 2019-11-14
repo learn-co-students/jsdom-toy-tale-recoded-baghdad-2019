977c4716842b3b80531e0220d085d91e2f05ca94
zozo <zahraa.jalaa99@gmail.com>
2019-11-14 23:28:55 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,18 +1,94 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const mainDiv = document.getElementById('toy-collection');
 let addToy = false
 
 // YOUR CODE HERE
+function gettingToys() {
+    return (
+        fetch('http://localhost:3000/toys')
+        .then(res => res.json())
+    )
+}
 
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-  } else {
-    toyForm.style.display = 'none'
-  }
+function createCardInfo(toy) {
+    let h2 = document.createElement('h2')
+    h2.innerText = toy.name
+    let img = document.createElement('img')
+    img.src = toy.image
+    img.classList.add('toy-avatar')
+    let p = document.createElement('p')
+    p.innerHTML = `${toy.likes} likes`;
+    let cardBtn = document.createElement('button')
+    cardBtn.classList.add('like-btn')
+    cardBtn.innerText = "like"
+    cardBtn.setAttribute('id', toy.id)
+    cardBtn.addEventListener('click', (e) => {
+        console.log(e.target.dataset);
+        likes(e)
+    })
+    let divCard = document.createElement('div')
+    divCard.setAttribute('class', 'card')
+    divCard.append(h2, img, p, cardBtn)
+    mainDiv.append(divCard)
+}
+gettingToys().then(toys => {
+    toys.forEach(toy => {
+        createCardInfo(toy)
+    })
 })
 
+function posttingToy(givenToy) {
+    fetch('http://localhost:3000/toys', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                "name": givenToy.name.value,
+                "image": givenToy.image.value,
+                "likes": 0
 
-// OR HERE!
+            })
+        })
+        .then(res => res.json())
+        .then((obj_toy) => {
+            let new_toy = createCardInfo(obj_toy)
+            mainDiv.append(new_toy)
+        })
+}
+addBtn.addEventListener('click', () => {
+    // hide & seek with the form
+    addToy = !addToy
+    if (addToy) {
+        toyForm.style.display = 'block'
+        toyForm.addEventListener('submit', event => {
+            event.preventDefault()
+            posttingToy(event.target)
+        })
+    } else {
+        toyForm.style.display = 'none'
+    }
+})
+
+function likes(e) {
+    e.preventDefault()
+    let updateLikes = parseInt(e.target.previousElementSibling.innerText) + 1
+
+    fetch(`http://localhost:3000/toys/${e.target.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+
+            },
+            body: JSON.stringify({
+                "likes": updateLikes
+            })
+        })
+        .then(res => res.json())
+        .then((like_obj => {
+            e.target.previousElementSibling.innerText = `${updateLikes} likes`;
+        }))
+}
